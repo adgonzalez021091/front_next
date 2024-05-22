7d1f7ae91ac33e2b439803ead1e8b796a908a239
ad.gonzalez021091@gmail.com <ad.gonzalez021091@gmail.com>
2024-05-22 16:27:50 -0500

    en uurl

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { headers } from 'next/headers'
+"use client"
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router';
 
 export default function page() {
-    const headersList = headers()
-    const referer = headersList.get('referer')
-    
-    return <div>Referer: {referer}</div>
+    const router = useRouter();
+
+    useEffect(() => {
+        const token = router.query.token as string;
+        if (token) {
+            localStorage.setItem('jwt_token', token);
+            router.push('/protected');  // Redirect to a protected page
+        }
+    }, [router]);
+    return <div>Referer: ok</div>
     
 }
