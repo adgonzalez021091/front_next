8bc8e39639e43ba963f892a368e70a6c7dc20281
ad.gonzalez021091@gmail.com <ad.gonzalez021091@gmail.com>
2024-05-22 16:21:40 -0500

    headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { headers } from 'next/headers'
 
 export default function page() {
-  return (
-    <div>
-      trabajando...
-    </div>
-  )
+    const headersList = headers()
+    const referer = headersList.get('referer')
+    
+    return <div>Referer: {referer}</div>
+    
 }
