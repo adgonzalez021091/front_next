23c9db848f0f4480a5060ecf371014c81796ad0c
ad.gonzalez021091@gmail.com <ad.gonzalez021091@gmail.com>
2024-05-23 16:15:13 -0500

    pa ver

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,10 +1,22 @@
-export default async (req, res) => {
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+type ResponseData = {
+  message: string
+}
+
+export async function GET(req: NextApiRequest,
+  res: NextApiResponse<ResponseData>) {
+    
     const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+    console.log(req.headers)
     if (token) {
         res.setHeader('Set-Cookie', `jwt_token=${token}; Path=/; HttpOnly; Secure; SameSite=Strict`);
         localStorage.setItem('jwt_token', token);
         res.redirect('/protected');
     } else {
-        res.status(400).json({ error: 'Token not found' });
+      return Response.json({ error: 'No token found' })
     }
 };
+
+ 
